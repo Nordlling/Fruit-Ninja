6aeecd46c8f46926e92eadbad9a69010c1dc0b56
Nordling <apodvorny@devsgroup.net>
2023-10-19 12:33:31 +0300

    Fix spawn position bug

--- Assets/Main/Scripts/Logic/Spawn/SpawnVisualizer.cs
+++ Assets/Main/Scripts/Logic/Spawn/SpawnVisualizer.cs
@@ -6,14 +6,44 @@ namespace Main.Scripts.Logic.Spawn
     {
         [SerializeField] private Spawner _spawner;
         [SerializeField] private Camera _camera;
+        
+        private Resolution _currentResolution;
 
         private void OnDrawGizmos()
         {
             foreach (SpawnInfo spawnInfo in _spawner.SpawnAreas)
             {
                 SetPosition(spawnInfo.spawnerAreaInfo);
-                DrawSpawnField(spawnInfo);
-                DrawAngles(spawnInfo);
+                DrawSpawnField(spawnInfo.spawnerAreaInfo);
+                DrawAngles(spawnInfo.spawnerAreaInfo);
+            }
+        }
+
+        private void Start()
+        {
+            _currentResolution = Screen.currentResolution;
+            UpdatePosition();
+        }
+
+        private void Update()
+        {
+            if (ResolutionIsChanged())
+            {
+                UpdatePosition();
+            }
+        }
+
+        private bool ResolutionIsChanged()
+        {
+            return Screen.currentResolution.width != _currentResolution.width ||
+                   Screen.currentResolution.height != _currentResolution.height;
+        }
+
+        private void UpdatePosition()
+        {
+            foreach (SpawnInfo spawnInfo in _spawner.SpawnAreas)
+            {
+                SetPosition(spawnInfo.spawnerAreaInfo);
             }
         }
 
@@ -32,24 +62,24 @@ namespace Main.Scripts.Logic.Spawn
             spawnerInfo._lastPoint = _camera.ScreenToWorldPoint(new Vector2(lastPositionX, lastPositionY));
         }
 
-        private void DrawAngles(SpawnInfo spawnInfo)
+        private void DrawAngles(SpawnerAreaInfo spawnInfo)
         {
             Gizmos.color = Color.yellow;
-            Vector2 firstPoint = spawnInfo.spawnerAreaInfo._firstPoint;
-            Vector2 lastPoint = spawnInfo.spawnerAreaInfo._lastPoint;
+            Vector2 firstPoint = spawnInfo._firstPoint;
+            Vector2 lastPoint = spawnInfo._lastPoint;
             Vector2 lineVector = lastPoint - firstPoint;
             Vector2 centerPoint = Vector2.Lerp(firstPoint, lastPoint, 0.5f);
             Vector2 normal = new Vector2(-lineVector.y, lineVector.x).normalized;
-            Vector2 rotatedLeftVector = Quaternion.Euler(0, 0, -(-spawnInfo.spawnerAreaInfo._leftAngle)) * normal;
-            Vector2 rotatedRightVector = Quaternion.Euler(0, 0, -(spawnInfo.spawnerAreaInfo._rightAngle)) * normal;
+            Vector2 rotatedLeftVector = Quaternion.Euler(0, 0, -(-spawnInfo._leftAngle)) * normal;
+            Vector2 rotatedRightVector = Quaternion.Euler(0, 0, -(spawnInfo._rightAngle)) * normal;
             Gizmos.DrawRay(centerPoint, rotatedLeftVector.normalized);
             Gizmos.DrawRay(centerPoint, rotatedRightVector.normalized);
         }
 
-        private void DrawSpawnField(SpawnInfo spawnInfo)
+        private void DrawSpawnField(SpawnerAreaInfo spawnInfo)
         {
             Gizmos.color = Color.blue;
-            Gizmos.DrawLine(spawnInfo.spawnerAreaInfo._firstPoint, spawnInfo.spawnerAreaInfo._lastPoint);
+            Gizmos.DrawLine(spawnInfo._firstPoint, spawnInfo._lastPoint);
         }
     }
 }

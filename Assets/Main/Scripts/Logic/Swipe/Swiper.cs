6475e64043a484b80d0f09c1ba113c291c22bd32
Nordling <apodvorny@devsgroup.net>
2023-10-22 03:14:11 +0300

    Upgrade swiper trail

--- Assets/Main/Scripts/Logic/Swipe/Swiper.cs
+++ Assets/Main/Scripts/Logic/Swipe/Swiper.cs
@@ -11,7 +11,8 @@ namespace Main.Scripts.Logic.Swipe
         public Vector2 Position => transform.position;
 
         [SerializeField] private float _minSpeedToSlice;
-        [SerializeField] private TrailRenderer _trailRenderer;
+        [SerializeField] private TrailRenderer _firstTrailRenderer;
+        [SerializeField] private TrailRenderer _secondTrailRenderer;
 
         private Camera _camera;
         private ITimeProvider _timeProvider;
@@ -50,12 +51,14 @@ namespace Main.Scripts.Logic.Swipe
         {
             if (!_stop && _timeProvider.GetTimeScale() != 0f && Input.GetMouseButton(0))
             {
-                _trailRenderer.enabled = true;
+                _firstTrailRenderer.enabled = true;
+                _secondTrailRenderer.enabled = true;
                 Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
                 transform.position = mousePosition;
                 if (!_touched)
                 {
-                    _trailRenderer.enabled = false;
+                    _firstTrailRenderer.enabled = false;
+                    _secondTrailRenderer.enabled = false;
                     _lastPosition = transform.position;
                 }
                 _touched = true;
@@ -64,6 +67,7 @@ namespace Main.Scripts.Logic.Swipe
             {
                 _touched = false;
             }
+            
             Speed = Vector2.Distance(_lastPosition, transform.position) / Time.deltaTime;
             Direction = transform.position - _lastPosition;
             _lastPosition = transform.position;

cb3ec269adbfb0155e480a0dfc9a7292ad297543
Nordling <apodvorny@devsgroup.net>
2023-10-23 07:54:02 +0300

    Refactoring

--- Assets/Main/Scripts/Infrastructure/Configs/HealthConfig.cs
+++ Assets/Main/Scripts/Infrastructure/Configs/HealthConfig.cs
@@ -5,7 +5,6 @@ namespace Main.Scripts.Infrastructure.Configs
     [CreateAssetMenu(fileName = "HealthConfig", menuName = "Configs/Health")]
     public class HealthConfig : ScriptableObject
     {
-        [Range(1, 8)]
         public int HealthCount = 3;
     }
 }
